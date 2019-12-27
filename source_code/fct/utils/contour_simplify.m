1eb97b3e17b8c24f8acd293817eb07e91b044e7d
Thomas Guillod <guillod@otvam.ch>
2019-12-27 17:43:48 +0100

    Handle contour that are removed when simplified

--- source_code/fct/utils/contour_simplify.m
+++ source_code/fct/utils/contour_simplify.m
@@ -1,10 +1,13 @@
-function c_cell = contour_simplify(c_cell, simplify_tol)
+function c_cell_out = contour_simplify(c_cell_in, simplify_tol)
 %CONTOUR_SIMPLIFY Simplify contour with a given angular tolerance.
-%   c_cell = contour_simplify(c_cell, simplify_tol)
-%   c_cell - cell array with the contours (cell)
-%      c_cell{i}.pts - contour points (matrix)
-%      c_cell{i}.is_cut - if the contour is a hole (boolean)
+%   c_cell_out = contour_simplify(c_cell_in, simplify_tol)
+%   c_cell_in - cell array with the contours (cell)
+%      c_cell_in{i}.pts - contour points (matrix)
+%      c_cell_in{i}.is_cut - if the contour is a hole (boolean)
 %   simplify_tol - angular tolerance for contour simplification (scalar)
+%   c_cell_out - cell array with the simplified contours (cell)
+%      c_cell_out{i}.pts - contour points (matrix)
+%      c_cell_out{i}.is_cut - if the contour is a hole (boolean)
 %
 %   See also CONTOUR_CREATE, REDUCEM.
 
@@ -12,11 +15,13 @@ function c_cell = contour_simplify(c_cell, simplify_tol)
 %   2019 - BSD License.
 
 % simplify all the contours
-for i=1:length(c_cell)
-    c_tmp = c_cell{i};
+c_cell_out = {};
+for i=1:length(c_cell_in)
+    c_tmp = c_cell_in{i};
     [x_tmp, y_tmp] = reducem(c_tmp.pts(:,1), c_tmp.pts(:,2), simplify_tol);
-    c_cell{i}.pts = [x_tmp, y_tmp];
-    c_cell{i}.is_cut = c_tmp.is_cut;
+    if (nnz(x_tmp)>0)&&(nnz(y_tmp)>0)
+        c_cell_out{end+1} = struct('pts', [x_tmp, y_tmp], 'is_cut', c_tmp.is_cut);
+    end
 end
 
 end
